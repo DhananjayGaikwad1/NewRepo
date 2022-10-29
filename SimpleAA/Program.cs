7620249bbc001ecc59df717760718767537857cb
DhananjayGaikwad1 <113286839+DhananjayGaikwad1@users.noreply.github.com>
2022-10-29 14:00:18 +0530

    Project upload

---
+++ SimpleAA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 9;
+            int c = a + b;
+            Console.WriteLine("Addition "+c);
+            Console.ReadLine();
+        }
+    }
+}
